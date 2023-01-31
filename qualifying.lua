41b22ed3c1b595f050f115a44342fc6ee96faed9
nicholas-thorpe <nicholas_thorpe@mhu.edu>
2023-01-30 22:03:34 -0500

    Initial commit

---
+++ qualifying.lua
@@ -0,0 +1,6 @@
+
+
+-- Import code for player data (opponents)
+local OpponentLibraries = require('Module:OpponentLibraries')
+local Opponent = OpponentLibraries.Opponent
+local OpponentDisplay = OpponentLibraries.OpponentDisplay
